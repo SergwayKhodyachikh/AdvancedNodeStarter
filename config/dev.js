c025c49d2debcaf96667a8e55ee0ca59f19511a1
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2020-04-18 19:47:00 +0300

    integrated caching with redis: create service/cache.js, middlewares/clearCache.js

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,6 @@
 module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  googleClientID: '574195523916-3je2fnbvqvk7kitembvga6g0h1emlk0t.apps.googleusercontent.com',
+  googleClientSecret: 'Bi6qQtt3sWHKSewhr_7pi75D',
+  mongoURI: 'mongodb+srv://sergway:Z45E65nOiCSswAJ3@advancednodestarter-nhsxi.mongodb.net/test',
   cookieKey: '123123123'
 };
